891c7a7c84fbd8fba6910f4f966b015eeec8acca
Gabriel Ferreira <gabreeiel@gmail.com>
2018-02-27 14:35:19 -0300

    Adicionando EJS como renderizador, fazendo rotas e criando algumas views

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express() //passo executando
 
+app.set('view engine', 'ejs') //definindo ejs como gerador das paginas
+
+//rotas
 app.get('/', (requisicao, resposta) => {
-    resposta.send('To funcionando carai!')
+    resposta.render('home/index') //em vez de .send, se usa .render (render acho que é do EJS)
+})
+app.get('/formulario_add_noticia', (requisicao, resposta) => {
+    resposta.render('admin/form_add_noticia')
+})
+app.get('/noticias', (requisicao, resposta) => {
+    resposta.render('noticias/noticias')
 })
 
 app.listen(3000,() => {
