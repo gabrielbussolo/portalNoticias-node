f6005dde038577ece371e03f3fe5f00790f353b6
Gabriel Ferreira <ferreira@MacBook-Pro-de-Gabriel.local>
2018-03-06 16:13:18 -0300

    Refact do banco de dados e instalacao do modulo consign

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,14 +1,9 @@
+const dbConnection = require('../../config/dbconnection')
+
 module.exports = app => {
     app.get('/noticias', (requisicao, resposta) => {
-        const mysql = require('mysql') // aqui eu to importando o modulo do MYSQL blz...
 
-        const connection = mysql.createConnection({ //createConnection recebe um objeto como parametro
-            host: 'localhost', //container docker com mysql
-            user: 'root',
-            password: 'admin123',
-            database: 'portal_noticias'
-        }) //beleza, to criando uma conexao, 
-        
+        const connection = dbConnection()
         connection.query('select * from noticias', (erro, resultado) => {
             if(erro){
                 console.log(erro)
---
+++ config/dbconnection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql') // aqui eu to importando o modulo do MYSQL blz...
+module.exports = () => {
+    return mysql.createConnection({ //createConnection recebe um objeto como parametro
+        host: 'localhost', //container docker com mysql
+        user: 'root',
+        password: 'admin123',
+        database: 'portal_noticias'
+    }) //beleza, to criando uma conexao,
+}
