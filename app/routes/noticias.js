14b95b4c6570ca433cbd491341fb865ff9ec1141
Gabriel Ferreira <ferreira@MacBook-Pro-de-Gabriel.local>
2018-03-06 15:25:03 -0300

    Conectando e exibindo dados do DB

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,22 @@
 module.exports = app => {
     app.get('/noticias', (requisicao, resposta) => {
-        resposta.render('noticias/noticias')
+        const mysql = require('mysql') // aqui eu to importando o modulo do MYSQL blz...
+
+        const connection = mysql.createConnection({ //createConnection recebe um objeto como parametro
+            host: 'localhost', //container docker com mysql
+            user: 'root',
+            password: 'admin123',
+            database: 'portal_noticias'
+        }) //beleza, to criando uma conexao, 
+        
+        connection.query('select * from noticias', (erro, resultado) => {
+            if(erro){
+                console.log(erro)
+            }
+            resposta.send(resultado)
+
+        })
+
+        //resposta.render('noticias/noticias')
     })
 }
